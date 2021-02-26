b4c373e1e603f045cd673066145012011859dcba
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2021-02-25 21:28:21 -0300

    Melhorias em desafios completos

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react'
+import { challengesContext } from '../contexts/ChallengeContext'
 import styles from '../styles/components/CompletedChallenges.module.css'
 
 export function CompletedChallenges(){
+    const { challengesCompleted } = useContext(challengesContext)
     return(
         <div className={styles.completedChallengesContainer}>
             <span>Desafios completos</span>
-            <span>5</span>
+            <span>{challengesCompleted}</span>
         </div>
     )
 }
