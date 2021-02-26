f8baecf74cfcf9db50f27647f30f640895dc74ee
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2021-02-25 21:28:39 -0300

    melhorias na experience bar

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react';
+import { challengesContext } from '../contexts/ChallengeContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar(){
+    const { currentExperience, experienceToNextLevel } = useContext(challengesContext);
+    const percentToNextLevel = Math.round((currentExperience * 100) / experienceToNextLevel);
     return(
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{width: '50%'}}>
-                <span className={styles.currentExperience} style={{left: '50%'}}>300px</span>
+                <div style={{width: `${percentToNextLevel}`}}>
+                <span className={styles.currentExperience} style={{left: `${percentToNextLevel}`}}>{currentExperience} xp</span>
                 </div>
             </div>
-            <span>600 xp</span>
+            <span>{ experienceToNextLevel } xp</span>
         </header>
     )
 }
