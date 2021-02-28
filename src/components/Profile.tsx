d3ec50cbed6ac19e259adb05e3a28ad544a0fc38
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2021-02-28 16:37:42 -0300

    Dia 5 finalizado

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { challengesContext } from '../contexts/ChallengeContext';
-import styles from '../styles/components/profile.module.css'
+import styles from '../styles/components/Profile.module.css'
 
 export function Profile(){
     const { level } = useContext(challengesContext)
